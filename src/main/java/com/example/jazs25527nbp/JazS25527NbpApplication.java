6fa19102d10fdf6b4d87d1d457f18decc51533b5
Maciej Uzarski <s25527@pjwstk.edu.pl>
2023-06-24 15:09:31 +0200

    init commit

---
+++ src/main/java/com/example/jazs25527nbp/JazS25527NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs25527nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS25527NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS25527NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jazs25527nbp/JazS25527NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs25527nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS25527NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
